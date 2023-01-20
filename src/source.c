a6dc5673ed261e4023ea5357f2105d5f1ae9ea67
Hwagonglo <cijang74@naver.com>
2023-01-20 14:00:03 +0000

    입출력: 3003

--- src/source.c
+++ src/source.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
-int change(int year)
-{
-	year -= 543;
-	return year;
-}
-
 int main()
 {
-	int boolgy_year;
-	int defualt_year;
-	scanf("%d", &boolgy_year);
+	int chess_pice[6]; // 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수
 	
-	defualt_year = change(boolgy_year);
+	for(int i = 0; i < 6; i++)
+	{
+		scanf("%d", &chess_pice[i]);
+	}
 	
-	printf("%d", defualt_year);
+	chess_pice[0] -= 1;
+	chess_pice[1] -= 1;
+	chess_pice[2] -= 2;
+	chess_pice[3] -= 2;
+	chess_pice[4] -= 2;
+	chess_pice[5] -= 8;
+	
+	for(int i = 0; i < 6; i++)
+	{
+		chess_pice[i] *= -1;
+		if(i == 5)
+		{
+			printf("%d", chess_pice[i]);
+		}
+		
+		else
+		{
+			printf("%d ", chess_pice[i]);
+		}
+	}
+	
 	return 0;
 }

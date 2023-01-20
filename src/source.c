7d6031cfcc704ac6b12a253f598402e2c29640d3
Hwagonglo <cijang74@naver.com>
2023-01-20 13:23:44 +0000

    입출력: 18108

--- src/source.c
+++ src/source.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int change(int year)
+{
+	year -= 543;
+	return year;
+}
+
 int main()
 {
-	printf("최초 커밋 테스트");
+	int boolgy_year;
+	int defualt_year;
+	scanf("%d", &boolgy_year);
+	
+	defualt_year = change(boolgy_year);
+	
+	printf("%d", defualt_year);
 	return 0;
 }

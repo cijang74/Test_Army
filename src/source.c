91547da86518bbcf77b9212a9d9dbc94510e3021
Hwagonglo <cijang74@naver.com>
2023-01-20 14:23:15 +0000

    입출력: 25083

--- src/source.c
+++ src/source.c
@@ -2,33 +2,20 @@
 
 int main()
 {
-	int chess_pice[6]; // 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수
-	
-	for(int i = 0; i < 6; i++)
-	{
-		scanf("%d", &chess_pice[i]);
-	}
-	
-	chess_pice[0] -= 1;
-	chess_pice[1] -= 1;
-	chess_pice[2] -= 2;
-	chess_pice[3] -= 2;
-	chess_pice[4] -= 2;
-	chess_pice[5] -= 8;
-	
-	for(int i = 0; i < 6; i++)
-	{
-		chess_pice[i] *= -1;
-		if(i == 5)
-		{
-			printf("%d", chess_pice[i]);
-		}
-		
-		else
-		{
-			printf("%d ", chess_pice[i]);
-		}
-	}
+	// LINE1
+	printf("         ,r'");
+	printf("\"7\n");
+	// LINE2
+	printf("r`-_   ,'  ,/\n");
+	// LINE3
+	printf(" \\. ");
+	printf("\". L_r'\n");
+	// LINE4
+	printf("   `~\\/\n");
+	// LINE5
+	printf("      |\n");
+	// LINE6
+	printf("      |");
 	
 	return 0;
 }

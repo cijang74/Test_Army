f9a738a751f8cc28f7f705270657d726bbe2d0e7
Hwagonglo <cijang74@naver.com>
2023-01-21 13:55:33 +0000

    반복문: 25304

--- src/source.c
+++ src/source.c
@@ -2,71 +2,31 @@
 
 int main()
 {
-	int dice[3];
+	int full_price;
+	int kind_of_buyed;
 	int price;
-	int large_data = 0;
-	int flag = 0;
+	int num;
+	int temp = 0;
 	
-	// 주사위값 입력
-	for(int i = 0; i < 3; i++)
+	scanf("%d", &full_price);
+	scanf("%d", &kind_of_buyed);
+	
+	for(int i = 0; i < kind_of_buyed; i++)
 	{
-		scanf("%d", &dice[i]);
-		if(large_data < dice[i])
-		{
-			large_data = dice[i];
-		}
+		scanf("%d", &price);
+		scanf("%d", &num);
+		temp += price * num;
 	}
 	
-	// 같은 눈이 3개일 경우
-	if(dice[0] == dice[1])
+	if(full_price == temp)
 	{
-		if(dice[1] == dice[2])
-		{
-			if(dice[0] == dice[2])
-			{
-				price = 10000 + dice[0] * 1000;
-				flag = 1;
-			}
-		}
+		printf("Yes");
 	}
 	
-	// 같은 눈이 없을 경우
-	else if(dice[0] != dice[1])
+	else
 	{
-		if(dice[1] != dice[2])
-		{
-			if(dice[0] != dice[2])
-			{
-				price = large_data * 100;
-				flag = 1;
-			}
-		}
+		printf("No");
 	}
 	
-	// 같은 눈이 2개일 경우
-	if(!flag)
-	{
-		printf("DEBUG");
-		int same_data;
-		
-		if(dice[0] == dice[1])
-		{
-			same_data = dice[0];
-		}
-		
-		else if(dice[1] == dice[2])
-		{
-			same_data = dice[1];
-		}
-		
-		else
-		{
-			same_data = dice[0];
-		}
-		
-		price = 1000 + same_data * 100;
-	}
-	
-	printf("%d", price);
-	return 0; 
+	return 0;
 }

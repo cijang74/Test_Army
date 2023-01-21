f3da5408927196c36bfcc718ba51d367e2950555
Hwagonglo <cijang74@naver.com>
2023-01-21 13:33:47 +0000

    조건문: 2480

--- src/source.c
+++ src/source.c
@@ -2,28 +2,71 @@
 
 int main()
 {
-	int hour;
-	int minuate;
-	int cook_time;
+	int dice[3];
+	int price;
+	int large_data = 0;
+	int flag = 0;
 	
-	scanf("%d", &hour);
-	scanf("%d", &minuate);
-	scanf("%d", &cook_time);
-	
-	minuate += cook_time;
-	
-	if(minuate >= 60)
+	// 주사위값 입력
+	for(int i = 0; i < 3; i++)
 	{
-		hour += minuate / 60;
-		minuate %= 60;
+		scanf("%d", &dice[i]);
+		if(large_data < dice[i])
+		{
+			large_data = dice[i];
+		}
 	}
 	
-	if(hour >= 24)
+	// 같은 눈이 3개일 경우
+	if(dice[0] == dice[1])
 	{
-		hour -= 24;
+		if(dice[1] == dice[2])
+		{
+			if(dice[0] == dice[2])
+			{
+				price = 10000 + dice[0] * 1000;
+				flag = 1;
+			}
+		}
 	}
 	
-	printf("%d %d", hour, minuate);
+	// 같은 눈이 없을 경우
+	else if(dice[0] != dice[1])
+	{
+		if(dice[1] != dice[2])
+		{
+			if(dice[0] != dice[2])
+			{
+				price = large_data * 100;
+				flag = 1;
+			}
+		}
+	}
 	
-	return 0;
+	// 같은 눈이 2개일 경우
+	if(!flag)
+	{
+		printf("DEBUG");
+		int same_data;
+		
+		if(dice[0] == dice[1])
+		{
+			same_data = dice[0];
+		}
+		
+		else if(dice[1] == dice[2])
+		{
+			same_data = dice[1];
+		}
+		
+		else
+		{
+			same_data = dice[0];
+		}
+		
+		price = 1000 + same_data * 100;
+	}
+	
+	printf("%d", price);
+	return 0; 
 }

a5d618bcb5c6c284b0efcdab1510ed1a20694cd3
Hwagonglo <cijang74@naver.com>
2023-01-22 14:32:29 +0000

    2차원 배열: 2566

--- src/source.c
+++ src/source.c
@@ -1,80 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-	int N,M;
-	scanf("%d", &N);
-	scanf("%d", &M);
+	int arr[10][10];
+	int max = -1;
+	int x,y;
 	
-	// 2차원 배열 동적할당 (행렬 A)
-	int **arr1 = (int**)malloc(sizeof(int*) * N);
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < 9; i++)
 	{
-		arr1[i] = (int*)malloc(sizeof(int) * M);
-	}
-	
-	// 2차원 배열 동적할당 (행렬 B)
-	int **arr2 = (int**)malloc(sizeof(int*) * N);
-	for(int i = 0; i < N; i++)
-	{
-		arr2[i] = (int*)malloc(sizeof(int) * M);
-	}
-	
-	// 2차원 배열 동적할당 (행렬 A+B를 저장할 행렬C)
-	int **arr3 = (int**)malloc(sizeof(int*) * N);
-	for(int i = 0; i < N; i++)
-	{
-		arr3[i] = (int*)malloc(sizeof(int) * M);
-	}
-	
-	for(int i = 0; i < N; i++) // 행렬A의 원소 입력
-	{
-		for(int j = 0; j < M; j++)
+		for(int j = 0; j < 9; j++)
 		{
-			scanf("%d", &arr1[i][j]);
+			scanf("%d", &arr[i][j]);
+			
+			if(arr[i][j] > max)
+			{
+				max = arr[i][j];
+				x = i+1;
+				y = j+1;
+			}
 		}
 	}
 	
-	for(int i = 0; i < N; i++) // 행렬B의 원소 입력
-	{
-		for(int j = 0; j < M; j++)
-		{
-			scanf("%d", &arr2[i][j]);
-			arr3[i][j] = arr1[i][j] + arr2[i][j];
-		}
-	}
-	
-	for(int i = 0; i < N; i++) // 행렬B의 원소 입력
-	{
-		for(int j = 0; j < M; j++)
-		{
-			printf("%d ", arr3[i][j]);
-		}
-		if(i < N-1)
-		{
-			printf("\n");
-		}
-	}
-	
-	// 동적 할당 해제
-	for(int i = 0; i < N; i++)
-	{
-		free(arr1[i]);
-	}
-	free(arr1);
-	
-	for(int i = 0; i < N; i++)
-	{
-		free(arr2[i]);
-	}
-	free(arr2);
-	
-	for(int i = 0; i < N; i++)
-	{
-		free(arr3[i]);
-	}
-	free(arr3);
+	printf("%d\n", max);
+	printf("%d %d", x, y);
 	
 	return 0;
 }

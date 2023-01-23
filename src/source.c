b19e76a7035470a2437eea310c34d87848f0c299
Hwagonglo <cijang74@naver.com>
2023-01-23 13:56:05 +0000

    2차원 배열 2563

--- src/source.c
+++ src/source.c
@@ -1,28 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	int arr[10][10];
-	int max = -1;
-	int x,y;
+	int white_paper[100][100];
+	int color_paper = 0;
+	int size = 0;
 	
-	for(int i = 0; i < 9; i++)
+	typedef struct Coordinate
 	{
-		for(int j = 0; j < 9; j++)
+		int x;
+		int y;
+	}coordinate;
+	
+	scanf("%d", &color_paper);
+	
+	coordinate* arr = (coordinate*)malloc(sizeof(coordinate) * color_paper);
+	
+	// 도화지 2차원 배열 초기화
+	for(int i = 0; i < 100; i++)
+	{
+		for(int j = 0; j <= 100; j++)
 		{
-			scanf("%d", &arr[i][j]);
-			
-			if(arr[i][j] > max)
+			white_paper[i][j] = 0;
+		}
+	}
+	
+	// 색종이의 xy좌표 입력
+	for(int i = 0; i < color_paper; i++)
+	{
+		scanf("%d", &arr[i].x);
+		scanf("%d", &arr[i].y);
+	}
+	
+	// 도화지에 색종이 그리기
+	for(int i = 0; i < color_paper; i++)
+	{
+		for(int j = arr[i].x; j < arr[i].x + 10; j++)
+		{
+			for(int k = arr[i].y; k < arr[i].y + 10; k++)
 			{
-				max = arr[i][j];
-				x = i+1;
-				y = j+1;
+				white_paper[j][k] = 1;
 			}
 		}
 	}
 	
-	printf("%d\n", max);
-	printf("%d %d", x, y);
+	// 도화지에 그린 색종이 크기 구하기
+	for(int i = 0; i < 100; i++)
+	{
+		for(int j = 0; j < 100; j++)
+		{
+			if(white_paper[i][j] == 1)
+			{
+				size++;
+			}
+		}
+	}
+	
+	free(arr);
+	
+	printf("%d", size);
 	
 	return 0;
 }

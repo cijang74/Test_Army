10fbb1ad78cb2e8af7c285d8f52d7559b2668063
Hwagonglo <cijang74@naver.com>
2023-01-28 14:11:11 +0000

    정렬: 25305

--- src/source.c
+++ src/source.c
@@ -2,47 +2,43 @@
 // https://hyo-ue4study.tistory.com/68
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	int numbers[6];
-	int min = 0;
-	int temp = 0;
-	int avg = 0;
-	int mdn = 0;
+	int people_num;
+	int award_num;
+	int min;
+	int temp;
+	int* arr;
 	
-	for(int i = 0; i < 5; i++)
+	scanf("%d", &people_num);
+	scanf("%d", &award_num);
+	
+	arr = (int*)malloc(sizeof(int) * people_num);
+	
+	for(int i = 0; i < people_num; i++)
 	{
-		scanf("%d", &numbers[i]);
-		avg += numbers[i];
+		scanf("%d", &arr[i]);
 	}
 	
-	// 선택 정렬 알고리즘: 오름차순 정렬
-	for(int i = 0; i < 4; i++)
+	for(int i = 0; i < people_num - 1; i++)
 	{
-		// 처음에 비교할 수를 min으로 지정한다.
 		min = i;
-		
-		// 처음에 비교할 수의 앞의 수 부터 탐색한다.
-		for(int j = i + 1; j < 5; j++)
+		for(int j = i + 1; j < people_num; j++)
 		{
-			// 만약 위에서 지정한 값보다 더 작은 수를 찾았다면 그 수로 min을 지정
-			if(numbers[j] < numbers[min])
+			if(arr[j] < arr[min])
 			{
 				min = j;
 			}
 		}
 		
-		temp = numbers[i];
-		numbers[i] = numbers[min];
-		numbers[min] = temp;
+		temp = arr[i];
+		arr[i] = arr[min];
+		arr[min] = temp;
 	}
 	
-	avg /= 5;
-	mdn = numbers[2];
-	
-	printf("%d\n", avg);
-	printf("%d", mdn);
+	printf("%d", arr[people_num - award_num]);
 	
 	return 0;
 }
